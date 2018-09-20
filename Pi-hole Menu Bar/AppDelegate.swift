3309259c71fb21090eab513dfd37022dbb33112a
Brian John Hanna <bjhanna728@gmail.com>
2018-09-20 16:16:08 -0400

    Initial Commit

---
+++ Pi-hole Menu Bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pi-hole Menu Bar
+//
+//  Created by Brian Hanna on 9/20/18.
+//  Copyright © 2018 Brian Hanna. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Pi-hole Menu Bar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pi-hole Menu Bar
+//
+//  Created by Brian Hanna on 9/20/18.
+//  Copyright © 2018 Brian Hanna. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
